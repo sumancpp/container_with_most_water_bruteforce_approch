0dcff2d704f345f9b495608bd76d4c6970599d42
Suman Maity <someone.cpp@proton.me>
2025-03-20 23:55:14 +0530

    container_with_most_water_bruteforce_approch.cpp

--- container_with_most_water_bruteforce_approch.cpp
+++ container_with_most_water_bruteforce_approch.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-     int height[]={1,8,6,2,5,4,8,3,7};
-     int size=9;
-     int maxWater=0;
-     for(int i=0;i<size;i++){
-        for(int j=i+1;j<size;j++){
-            int w=j-i;
-            int ht=min(height[i],height[j]);
-            int currWater=w*ht;
-            maxWater=max(maxWater,currWater);
+     int height[]={1, 8, 6, 2, 5, 4, 8, 3, 7};
+     int size = 9;
+     int maxWater = 0;
+     for(int i = 0; i < size; i++){
+        for(int j = i + 1; j < size; j++){
+            int w = j - i;
+            int ht = min(height[i], height[j]);
+            int currWater = w * ht;
+            maxWater = max(maxWater, currWater);
         }
      }
      cout<<maxWater;
